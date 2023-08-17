aee001664fe213898160ceec81fb5617f48c9493
maheshmula <maheshmula617@gmail.com>
2023-08-17 13:09:17 +0530

    Text Modified

--- ForLoop.java
+++ ForLoop.java
@@ -3,7 +3,7 @@ package com.kn.forloop;
 public class ForLoop {
  public static void main(String[] args) {
 	for(int i=1;i<=5;i++) {
-		System.out.println("I shall not talk in class");
+		System.out.println("I shall talk in class");
 	}
 }
 }
